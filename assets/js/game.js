dc96329dade1099461860b86646fb36ff113f8fb
Ryan Kahlbaugh <gitworkcoding22@gmail.com>
2022-06-06 10:41:13 -0500

    added some js consoloe.log code

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,21 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function named "fight"
+
+function fight () {
+    window.alert("The fight has begun!");
+}
+
+var playerName = window.prompt("What is your robot's name?");
+// Note the lack of quotation marks around playerName
+//what is this?
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+// This will do math and log 20
+
+console.log(10 + 10);
+
+// What is this?
+console.log("Our robot's name is " + playerName);
+window.alert(playerName);
+
+// fight ();
